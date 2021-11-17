0011c6dd79d717e813ba9c7e26cb3a0d7d55e55b
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-18 00:04:52 +0900

    AppRouterをfeatures/pageに移動

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useAuthChange from '~/features/auth/hooks/useAuthChange'
-import AppRouter from '~/features/router/components/AppRouter'
+import AppRouter from '~/features/page/components/AppRouter'
 
 const App = () => {
   useAuthChange()
