4d626a0f1583edc8071c15c581b2f3172d59ff6a
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-22 22:05:20 +0900

    sequelize/models/index.cjsを修正

--- sequelize/models/index.cjs
+++ sequelize/models/index.cjs
@@ -16,7 +16,7 @@ if (config.use_env_variable) {
 
 fs.readdirSync(__dirname)
   .filter((file) => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.cjs'
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-4) === '.cjs'
   })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
