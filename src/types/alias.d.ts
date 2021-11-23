b311e0f61dc6ac3b96b194cf31368d96dff61040
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-24 00:25:18 +0900

    エイリアスを定義(パラメータの意味付けと判別が目的)

---
+++ src/types/alias.d.ts
@@ -0,0 +1,17 @@
+/**
+ * 型のエイリアスを定義する
+ */
+declare namespace Alias {
+  /**
+   * SupabaseのAuthenticationで発行されるUserUIDのエイリアス
+   */
+  type UserUID = string
+  /**
+   * 日付の文字列のエイリアス
+   */
+  type DateString = string
+  /**
+   * UUID v4のエイリアス
+   */
+  type UUIDV4 = string
+}
