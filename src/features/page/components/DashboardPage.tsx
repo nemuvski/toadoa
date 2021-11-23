457e387e1e773a46292901977beebc6e6f0f0e9b
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-23 14:58:58 +0900

    ダッシュボードのタイトル設定

--- src/features/page/components/DashboardPage.tsx
+++ src/features/page/components/DashboardPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import usePageTitle from '~/features/page/hooks/usePageTitle'
 
 const DashboardPage = () => {
+  usePageTitle('Dashboard')
+
   return <div>It works!</div>
 }
 
