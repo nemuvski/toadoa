682db82205fcd1f8502e4168cd2e620ede7c6992
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-11-24 00:26:11 +0900

    エンティティのベースの型を定義

---
+++ src/models/EntityBase.ts
@@ -0,0 +1,34 @@
+import { Dayjs } from 'dayjs'
+import dayjs from '~/libs/Dayjs'
+
+/**
+ * アプリケーション上で扱うエンティティベース
+ */
+export interface EntityBase {
+  id: Alias.UUIDV4
+  createdAt: Dayjs
+  updatedAt: Dayjs
+}
+
+/**
+ * DB上のデータのエンティティベース
+ */
+export interface DatabaseEntityBase {
+  id: Alias.UUIDV4
+  createdAt: Alias.DateString
+  updatedAt: Alias.DateString
+}
+
+/**
+ * EntityBaseのオブジェクトを返却する
+ *
+ * @param response Supabaseから取得したエンティティデータ
+ */
+export const buildEntityBase = (response: DatabaseEntityBase) => {
+  const { id, createdAt, updatedAt } = response
+  return {
+    id,
+    createdAt: dayjs(createdAt),
+    updatedAt: dayjs(updatedAt),
+  }
+}
