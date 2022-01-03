df664e3aa3a3a65673b8a4e3dff51d13bf0bad0f
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-01-03 18:43:26 +0900

    NotFoundPageの内容変更

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import usePageTitle from '~/hooks/usePageTitle'
+import { Card, CardBody } from '~/components/styled/Card'
+import { CenteringFrameHeading } from '~/components/styled/CenteringFrame'
+import { Circle } from '~/components/styled/Circle'
+import { Paragraph } from '~/components/styled/Paragraph'
 
 const NotFoundPage = () => {
-  usePageTitle('Not found')
+  usePageTitle('Page not found')
 
-  return <div>The link you followed probably broken, or the page has been removed.</div>
+  return (
+    <>
+      <CenteringFrameHeading>
+        <Circle size='large'>👻</Circle>
+      </CenteringFrameHeading>
+      <Card>
+        <CardBody>
+          <Paragraph>The link you followed probably broken, or the page has been removed.</Paragraph>
+        </CardBody>
+      </Card>
+    </>
+  )
 }
 
 export default NotFoundPage
