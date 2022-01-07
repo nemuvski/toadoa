ff5fdbd539b2076ef01317c1119510c4da5c84c4
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-01-07 18:02:17 +0900

    QueryClientの設定を追記

--- src/libs/react-query.ts
+++ src/libs/react-query.ts
@@ -4,7 +4,12 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       suspense: true,
-      retry: 0,
+      retry: 1,
+      cacheTime: 1000 * 60 * 15,
+      staleTime: 1000 * 60 * 5,
+    },
+    mutations: {
+      retry: 1,
     },
   },
 })
