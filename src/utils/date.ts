b537df2933af7d9653ba7bf9f91664517e59cefa
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-01-11 18:46:33 +0900

    dayjsのtimezone対応

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,13 +1,5 @@
 import { Dayjs } from 'dayjs'
-import dayjs from '~/libs/dayjs'
-
-export function getSavingDateString(dateString: Alias.DateString | null) {
-  if (!dateString) {
-    return null
-  }
-  return dayjs(dateString).utc().format()
-}
 
 export function formatDateYMD(date: Dayjs) {
-  return date.utc().format('YYYY-MM-DD')
+  return date.tz().format('YYYY-MM-DD')
 }
