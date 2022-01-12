c7584d7295d4ced7b66cf7b2cdad52225d4a5640
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-01-12 16:20:33 +0900

    CardSkeletonの内容を変更

--- src/components/CardListSkeleton.tsx
+++ src/components/CardListSkeleton.tsx
@@ -9,13 +9,14 @@ const CardSkeleton = () => {
         <ContentLoader
           speed={1}
           width='100%'
-          height={28}
+          height={68}
           backgroundColor='var(--color-gray-main)'
           foregroundColor='var(--color-white)'
           foregroundOpacity={0.1}
         >
-          <rect x='0' y='2' rx='3' ry='3' width='100%' height='8' />
-          <rect x='0' y='20' rx='3' ry='3' width='60%' height='8' />
+          <rect x='78%' y='2' rx='3' ry='3' width='22%' height='30' />
+          <rect x='0' y='42' rx='3' ry='3' width='100%' height='8' />
+          <rect x='0' y='60' rx='3' ry='3' width='60%' height='8' />
         </ContentLoader>
       </CardBody>
     </Card>
